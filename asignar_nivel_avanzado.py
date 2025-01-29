761a3fb6a2359ecb203ed9152de91d194b52f683
gruv2 <zmillanpaula@gmail.com>
2025-01-29 17:22:55 -0300

    problemas de importación

--- asignar_nivel_avanzado.py
+++ asignar_nivel_avanzado.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import logging
-from app.selenium_manager import tomar_screenshot  # 🔹 Importamos la función desde selenium_manager.py
+from .selenium_manager import tomar_screenshot  
 
 def asignar_nivel_avanzado(driver, correo, nivel):
     """
