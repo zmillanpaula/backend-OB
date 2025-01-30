ec124d47b731a2d9e5dfe9a338f19520e90d63ac
gruv2 <zmillanpaula@gmail.com>
2025-01-30 13:38:17 -0300

    importaciones selenium_manager

--- selenium_manager.py
+++ selenium_manager.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import logging
 import requests
 import os
